814cd52dca1a05176109aabc1965906e1c76e574
augusto cavalcante <gutocbp@gmail.com>
2026-05-03 01:50:03 -0300

    camera_and_bunker

---
+++ p2code/Camera.py
@@ -0,0 +1,81 @@
+import glfw
+import glm
+import numpy as np
+
+
+class Camera:
+    def __init__(self, largura, altura):
+        self.largura = largura
+        self.altura = altura
+
+        self.pos   = glm.vec3(0.0, 0.0, 3.0)
+        self.front = glm.vec3(0.0, 0.0, -1.0)
+        self.up    = glm.vec3(0.0, 1.0, 0.0)
+
+        self.yaw   = -90.0
+        self.pitch = 0.0
+        self.lastX = largura / 2.0
+        self.lastY = altura / 2.0
+        self.fov   = 45.0
+
+        self.firstMouse = True
+        self.deltaTime  = 0.0
+        self.lastFrame  = 0.0
+
+    def key_event(self, window, key, scancode, action, mods):
+        if key == glfw.KEY_ESCAPE and action == glfw.PRESS:
+            glfw.set_window_should_close(window, True)
+
+        cameraSpeed = 10 * self.deltaTime
+
+        if key == glfw.KEY_W and (action == glfw.PRESS or action == glfw.REPEAT):
+            self.pos += cameraSpeed * self.front
+
+        if key == glfw.KEY_S and (action == glfw.PRESS or action == glfw.REPEAT):
+            self.pos -= cameraSpeed * self.front
+
+        if key == glfw.KEY_A and (action == glfw.PRESS or action == glfw.REPEAT):
+            self.pos -= glm.normalize(glm.cross(self.front, self.up)) * cameraSpeed
+
+        if key == glfw.KEY_D and (action == glfw.PRESS or action == glfw.REPEAT):
+            self.pos += glm.normalize(glm.cross(self.front, self.up)) * cameraSpeed
+
+    def mouse_callback(self, window, xpos, ypos):
+        if self.firstMouse:
+            self.lastX = xpos
+            self.lastY = ypos
+            self.firstMouse = False
+
+        xoffset = (xpos - self.lastX) * 0.05
+        yoffset = (self.lastY - ypos) * 0.05
+        self.lastX = xpos
+        self.lastY = ypos
+
+        self.yaw   += xoffset
+        self.pitch += yoffset
+        self.pitch  = max(-89.0, min(89.0, self.pitch))
+
+        front = glm.vec3(
+            glm.cos(glm.radians(self.yaw)) * glm.cos(glm.radians(self.pitch)),
+            glm.sin(glm.radians(self.pitch)),
+            glm.sin(glm.radians(self.yaw)) * glm.cos(glm.radians(self.pitch))
+        )
+        self.front = glm.normalize(front)
+
+    def scroll_callback(self, window, xoffset, yoffset):
+        self.fov = max(1.0, min(45.0, self.fov - yoffset))
+
+    def framebuffer_size_callback(self, window, largura, altura):
+        self.largura = largura
+        self.altura  = altura
+        glViewport(0, 0, largura, altura)
+
+    def tick(self, current_time):
+        self.deltaTime = current_time - self.lastFrame
+        self.lastFrame = current_time
+
+    def get_view(self):
+        return np.array(glm.lookAt(self.pos, self.pos + self.front, self.up))
+
+    def get_projection(self):
+        return np.array(glm.perspective(glm.radians(self.fov), self.largura / self.altura, 0.1, 100.0))
--- p2code/Main.py
+++ p2code/Main.py
@@ -3,191 +3,58 @@ import glfw
 from OpenGL.GL import *
 import OpenGL.GL.shaders
 import numpy as np
-import glm
-import math
 import Loader
 import Object
-
+import Camera
 
 altura = 700
 largura = 700
 
 program, window = Setter.set(altura, largura)
 
-cameraPos   = glm.vec3(0.0, 0.0, 3.0)
-cameraFront = glm.vec3(0.0, 0.0, -1.0)
-cameraUp    = glm.vec3(0.0, 1.0, 0.0)
+camera = Camera.Camera(largura, altura)
 
-firstMouse = True
-yaw   = -90.0	# yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
-pitch =  0.0
-lastX =  largura / 2.0
-lastY =  altura / 2.0
-fov   =  45.0
-
-# timing
-deltaTime = 0.0	# time between current frame and last frame
-lastFrame = 0.0
-
-
-firstMouse = True
-yaw = -90.0 
-pitch = 0.0
-lastX =  largura/2
-lastY =  altura/2
-
-
-def key_event(window,key,scancode,action,mods):
-    global cameraPos, cameraFront, cameraUp, polygonal_mode
-
-    if key == glfw.KEY_ESCAPE and action == glfw.PRESS:
-        glfw.set_window_should_close(window, True)
-    
-    cameraSpeed = 10 * deltaTime
-    if key == glfw.KEY_W and (action == glfw.PRESS or action == glfw.REPEAT):
-        cameraPos += cameraSpeed * cameraFront
-    
-    if key == glfw.KEY_S and (action == glfw.PRESS or action == glfw.REPEAT):
-        cameraPos -= cameraSpeed * cameraFront
-    
-    if key == glfw.KEY_A and (action == glfw.PRESS or action == glfw.REPEAT):
-        cameraPos -= glm.normalize(glm.cross(cameraFront, cameraUp)) * cameraSpeed
-        
-    if key == glfw.KEY_D and (action == glfw.PRESS or action == glfw.REPEAT):
-        cameraPos += glm.normalize(glm.cross(cameraFront, cameraUp)) * cameraSpeed
-
-    if key == glfw.KEY_P and action == glfw.PRESS:
-        polygonal_mode = not polygonal_mode
-        
-
-def framebuffer_size_callback(window, largura, altura):
-
-    # make sure the viewport matches the new window dimensions note that width and 
-    # height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, largura, altura)
-
-# glfw: whenever the mouse moves, this callback is called
-# -------------------------------------------------------
-def mouse_callback(window, xpos, ypos):
-    global cameraFront, lastX, lastY, firstMouse, yaw, pitch
-   
-    if (firstMouse):
-
-        lastX = xpos
-        lastY = ypos
-        firstMouse = False
-
-    xoffset = xpos - lastX
-    yoffset = lastY - ypos # reversed since y-coordinates go from bottom to top
-    lastX = xpos
-    lastY = ypos
-
-    sensitivity = 0.05 # change this value to your liking
-    xoffset *= sensitivity
-    yoffset *= sensitivity
-
-    yaw += xoffset
-    pitch += yoffset
-
-    # make sure that when pitch is out of bounds, screen doesn't get flipped
-    if (pitch > 89.0):
-        pitch = 89.0
-    if (pitch < -89.0):
-        pitch = -89.0
-
-    front = glm.vec3()
-    front.x = glm.cos(glm.radians(yaw)) * glm.cos(glm.radians(pitch))
-    front.y = glm.sin(glm.radians(pitch))
-    front.z = glm.sin(glm.radians(yaw)) * glm.cos(glm.radians(pitch))
-    cameraFront = glm.normalize(front)
-
-# glfw: whenever the mouse scroll wheel scrolls, this callback is called
-# ----------------------------------------------------------------------
-def scroll_callback(window, xoffset, yoffset):
-    global fov
-
-    fov -= yoffset
-    if (fov < 1.0):
-        fov = 1.0
-    if (fov > 45.0):
-        fov = 45.0
-    
-glfw.set_key_callback(window,key_event)
-glfw.set_framebuffer_size_callback(window, framebuffer_size_callback)
-glfw.set_cursor_pos_callback(window, mouse_callback)
-glfw.set_scroll_callback(window, scroll_callback)
-
-# tell GLFW to capture our mouse
+glfw.set_key_callback(window, camera.key_event)
+glfw.set_framebuffer_size_callback(window, camera.framebuffer_size_callback)
+glfw.set_cursor_pos_callback(window, camera.mouse_callback)
+glfw.set_scroll_callback(window, camera.scroll_callback)
 glfw.set_input_mode(window, glfw.CURSOR, glfw.CURSOR_DISABLED)
 
-
-
-
-
-def view():
-    global cameraPos, cameraFront, cameraUp
-    mat_view = glm.lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
-    mat_view = np.array(mat_view)
-    return mat_view
-
-def projection():
-    global altura, largura
-    # perspective parameters: fovy, aspect, near, far
-    mat_projection = glm.perspective(glm.radians(fov), largura/altura, 0.1, 100.0)
-
-    
-    mat_projection = np.array(mat_projection)    
-    return mat_projection
-
-
-
-
 loader = Loader.Loader(program)
-
-caixastart, caixaqt, caixatexture =loader.load_obj_and_texture('objetos/caixa/caixa.obj', ['objetos/caixa/caixa.jpg'])
-caixa = Object.Object(caixastart, caixaqt, caixatexture,program)
-
+caixastart, caixaqt, caixatexture = loader.load_obj_and_texture('objetos/bunker/bunker.obj', ['objetos/bunker/bunker.jpg'])
+caixa = Object.Object(caixastart, caixaqt, caixatexture, program)
 loader.upload()
 
+glEnable(GL_DEPTH_TEST)
+polygonal_mode = False
 
-
-
-
-glEnable(GL_DEPTH_TEST) ### importante para 3D
-polygonal_mode = False 
-
-
+# wrap key_event to also handle polygonal mode toggle
+original_key_event = camera.key_event
+def key_event(window, key, scancode, action, mods):
+    global polygonal_mode
+    original_key_event(window, key, scancode, action, mods)
+    if key == glfw.KEY_P and action == glfw.PRESS:
+        polygonal_mode = not polygonal_mode
+glfw.set_key_callback(window, key_event)
 
 glfw.show_window(window)
 while not glfw.window_should_close(window):
+    camera.tick(glfw.get_time())
+    glfw.poll_events()
 
-    currentFrame = glfw.get_time()
-    deltaTime = currentFrame - lastFrame
-    lastFrame = currentFrame
-
-    glfw.poll_events() 
-       
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    
     glClearColor(1.0, 1.0, 1.0, 1.0)
-    
-    if polygonal_mode:
-        glPolygonMode(GL_FRONT_AND_BACK,GL_LINE)
-    else:
-        glPolygonMode(GL_FRONT_AND_BACK,GL_FILL)
 
-    mat_view = view()
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE if polygonal_mode else GL_FILL)
+
     loc_view = glGetUniformLocation(program, "view")
-    glUniformMatrix4fv(loc_view, 1, GL_TRUE, mat_view)
+    glUniformMatrix4fv(loc_view, 1, GL_TRUE, camera.get_view())
 
-    mat_projection = projection()
     loc_projection = glGetUniformLocation(program, "projection")
-    glUniformMatrix4fv(loc_projection, 1, GL_TRUE, mat_projection)   
+    glUniformMatrix4fv(loc_projection, 1, GL_TRUE, camera.get_projection())
 
-    caixa.draw(0.0, 0, 0, 1, 0, 0, -20, 1.5, 1.5, 1.5, np.eye(4,4))
+    caixa.draw(0.0, 0, 0, 1, 0, 0, -20, 1.5, 1.5, 1.5, np.eye(4, 4))
 
-
-    
     glfw.swap_buffers(window)
 
 glfw.terminate()
